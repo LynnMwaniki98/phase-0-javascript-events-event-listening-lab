048fb9cf5a6bb505c32a2aaa0130b457cf1f171f
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-22 11:09:55 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function addingEventListener() {  
 
+  //First, we grab the element that we want to add the event listener to and save a reference to it in the input variable.
 const input = document.getElementById('button');
 
+//Next, we call addEventListener() on that element to tell JavaScript to listen for the event. We pass two arguments to addEventListener(): the name of the event to listen for (in this case, click) and a callback function that will be executed when the event is "heard."
 input.addEventListener('click', function() {
   alert("binds an event listener in addingEventListener()");
 })
